8e5e65104fc035344097d2adfef61fe7f4bdb07e
Prapanchj <145548350+Prapanchj@users.noreply.github.com>
2023-10-04 15:11:07 +0530

    Add files via upload

---
+++ sumofarray.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int sum(int arr[], int lim)
+{
+    int sum=0;
+    for (int i=0; i<lim; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+void main(){
+int lim, i;
+
+    printf("Enter the limit ");
+    scanf("%d", &lim);
+
+    int arr[lim];
+    printf("Enter %d numbers: ", lim);
+    for (i=0; i<lim; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Sum of numbers in array is %d\n", sum(arr,lim));
+}
+
+
